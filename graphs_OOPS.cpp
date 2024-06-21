59cc514e527503d4e6020d118a5afd6a1a44c829
Harsh Vardhan Gupta <harsh247@gmail.com>
2024-06-21 11:21:43 +0530

    added topSort(directed unweighted) and isCyclic(undirected unweighted)

--- graphs_OOPS.cpp
+++ graphs_OOPS.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printVector(vector<int>&v){
-    for(int i=0; i<v.size(); i++){
+void printVector(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
         cout << v[i] << " ";
     }
     cout << endl;
@@ -17,6 +19,7 @@ private:
 protected:
     //  making this protected as numVertices will be common for all graph types
     int numVertices;
+    bool isCyclicHelper(int node, vector<int>&visited, int parent);
 
 public:
     Graph(int vertices); // constructor
@@ -27,8 +30,11 @@ public:
     virtual void display();
     virtual vector<int> bfs(int vertex);
     virtual vector<int> dfs(int vertex);
-    // FOR YOU----> // used call by reference, for just using the address, instead of unnecessary copying 
-    virtual void explore(int vertex, vector<int>& visited, vector<int>& ans);
+
+    // FOR YOU----> // used call by reference, for just using the address, instead of unnecessary copying
+    virtual void explore(int vertex, vector<int> &visited, vector<int> &ans);
+
+    virtual bool isCyclic();
     // this way only DerictedGraph class can access adjList
     friend class DirectedGraph;
 };
@@ -84,7 +90,7 @@ vector<int> Graph::bfs(int vertex)
     }
     return ans;
 }
-void Graph::explore(int vertex, vector<int>& visited, vector<int>& ans)
+void Graph::explore(int vertex, vector<int> &visited, vector<int> &ans)
 {
     visited[vertex] = 1;
     ans.push_back(vertex);
@@ -98,16 +104,41 @@ void Graph::explore(int vertex, vector<int>& visited, vector<int>& ans)
 }
 vector<int> Graph::dfs(int vertex)
 {
-    vector <int> ans;
-    vector <int> visited(numVertices, 0);
+    vector<int> ans;
+    vector<int> visited(numVertices, 0);
     visited[vertex] = 1;
     ans.push_back(vertex);
-    for (auto neighbour : adjList[vertex]) 
+    for (auto neighbour : adjList[vertex])
     {
         explore(neighbour, visited, ans);
     }
     return ans;
 }
+bool Graph::isCyclicHelper(int node, vector<int>& visited, int parent){
+    visited[node] = 1;
+    for(auto neighbour : adjList[node]){
+        if(!visited[neighbour]){
+            if(isCyclicHelper(neighbour, visited, node)){
+                return true;
+            }
+        }else if(neighbour != parent){
+            return true;
+        }
+    }
+    return false;
+}
+bool Graph::isCyclic(){
+    vector<int> visited(numVertices, 0);
+    for(int node = 0; node < numVertices; node++){
+        if(!visited[node]){
+            if(isCyclicHelper(node, visited, -1)==true){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 class DirectedGraph : public Graph
 {
 public:
@@ -115,6 +146,8 @@ public:
     void addEdge(int v, int w);
     void removeEdge(int v, int w);
     // display function of Graph class will be used
+    // topological sort using Kahn algorithm
+    virtual vector<int> topoSort();
 };
 DirectedGraph::DirectedGraph(int vertices) : Graph(vertices) {}
 void DirectedGraph::addEdge(int v, int w)
@@ -124,6 +157,42 @@ void DirectedGraph::addEdge(int v, int w)
 void DirectedGraph::removeEdge(int v, int w)
 {
     adjList[v].erase(find(adjList[v].begin(), adjList[v].end(), w));
+}
+
+vector<int> DirectedGraph::topoSort()
+{
+    vector<int> ans;
+    vector<int> indegree(numVertices, 0);
+    queue<int> q;
+    for (int node = 0; node < numVertices; node++)
+    {
+        for (auto neighbour : adjList[node])
+        {
+            indegree[neighbour]++;
+        }
+    }
+    for (int node = 0; node < numVertices; node++)
+    {
+        if (indegree[node] == 0)
+        {
+            q.push(node);
+        }
+    }
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        ans.push_back(node);
+        for (auto neighbour : adjList[node])
+        {
+            indegree[neighbour]--;
+            if (indegree[neighbour] == 0)
+            {
+                q.push(neighbour);
+            }
+        }
+    }
+    return ans;
 }
 
 class WeightedGraph : public Graph
@@ -209,12 +278,13 @@ void WeightedDirectedGraph::removeEdge(int v, int w)
 
 int main()
 {
-    Graph g(4);
+    Graph g(3);
     g.addEdge(0, 1);
-    g.addEdge(0, 3);
-    g.addEdge(2, 3);
-    vector<int> ans = g.bfs(0);
-    printVector(ans);
+    g.addEdge(2, 1);
+    g.addEdge(2, 0);
+
+    bool ans = g.isCyclic();
+    cout << ans << endl;
     g.display();
     return 0;
 }
